dfad6ce3674307a0b9a5678118f471bc96605a32
zenodeapp <zenodeapp@gmail.com>
2024-04-08 16:40:58 +0000

    Progress with overall swap flow

--- modules/create-pool.sh
+++ modules/create-pool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source default variables
-source ./_default_variables.sh
+source ../_default_variables.sh
 
 # Function to create a json file for a 5:1 pool for OSMO:NAAN
 create_pool_json() {
@@ -21,12 +21,19 @@ create_pool_json() {
 
 echo "Welcome to the Osmosis Pool Creation Wizard!"
 read -p "Enter the amount of uosmo you'll deposit (1000000 = 1OSMO): " uosmo_amount
-read -p "Enter the ibc denomination for naan (ibc/1EAE32...) [default: $IBC_DENOM]: " naan_denom
+read -p "Enter the ibc denomination for naan (ibc/1EAE32...) [default: $NAAN_IBC_DENOM]: " naan_denom
 read -p "Enter the amount of $naan_denom to deposit: " naan_amount
+
+# Set defaults
+if [ -z "$naan_denom" ]; then
+  naan_denom="$NAAN_IBC_DENOM"
+fi
 
 # Create the pool JSON and create the pool
 create_pool_json "$uosmo_amount" "$naan_denom" "$naan_amount"
-osmosisd tx gamm create-pool --chain-id osmo-test-5 --pool-file ./pool.json --from osmrel --yes
+osmosisd tx gamm create-pool --chain-id osmo-test-5 --pool-file ./pool.json --from $OSMO_KEY --log_format json --yes
+
+# TODO: We need to capture the created pool ID and whatever details that are important
 
 create_pool_data_json() {
   # TODO: create a file containing info like the ibc/denom, the channel we're using etc.
--- shielded-swap.sh
+++ shielded-swap.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+# Source default variables
+source ./_default_variables.sh
+
 # Read pool data from pool-data.json
 
 # 1. Send naan from Namada to Osmosis
 read -p "Which transparent address (should be shielded, but doesn't work in SE) are you sending this from?: " namada_address
 read -p "Which shielded address (znam...) should receive this?: " shielded_namada_address
 read -p "Which osmosis address would you like to send this to (for transparency use a dummy address): " osmosis_address
-read -p "Enter the amount of $naan_denom to send: " naan_amount
+read -p "Enter the amount of naan to send: " naan_amount
 
 # Check osmosis address balance of the naan token (for the specified channel)
 
-namada client ibc-transfer --source $namada_address --receiver $osmosis_address --token naan --amount $naan_amount --channel-id channel-1235
+namada client ibc-transfer --source $namada_address --receiver $osmosis_address --token naan --amount $naan_amount --channel-id "channel-$COUNTER_CHANNEL_ID"
 
 # Loop to check if osmosis_address received the amount, timeout in 2 min
 
 # 2. Swap naan to osmo in the created pool when osmosis_address received the amount
 
+# AMOUNT RECEIVED
+$AMOUNT_RECEIVED=10
+
 # 3. Generate IBC memo and extract it
+TEMP_PATH=./temp/
+namada client ibc-gen-shielded --target "$shielded_namada_address" --channel-id "channel-$COUNTER_CHANNEL_ID" --token "uosmo" --amount $naan_amount --output-folder-path $TEMP_PATH
+# If we're sending NAAN instead, add this if we have enough time:
+# namada client ibc-gen-shielded --target "$shielded_namada_address" --channel-id "channel-$COUNTER_CHANNEL_ID" --token "transfer/$CHANNEL_ID/tnam1qxvg64psvhwumv3mwrrjfcz0h3t3274hwggyzcee" --amount $naan_amount --output-folder-path  $TEMP_PATH
+$IBC_MEMO=24042
 
 # 4. Send received osmosis amount back to namada_address
+osmosisd tx ibc-transfer transfer transfer "channel-$CHANNEL_ID" $shielded_namada_address "$AMOUNT_RECEIVED"uosmo --memo $IBC_MEMO --from $OSMO_KEY --node https://osmosis-testnet-rpc.polkachu.com:443 --chain-id osmo-test-5 --fees 1000uosmo
 
 # 5. Check if expected osmo is received
