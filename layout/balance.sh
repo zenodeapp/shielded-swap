202305bed0e9ba4b0cf58573ad97578c40cd63ed
zenodeapp <zenodeapp@gmail.com>
2024-04-11 23:40:46 +0200

    naan hardcoded string to

--- layout/balance.sh
+++ layout/balance.sh
@@ -27,14 +27,14 @@ print_balance_block_value() {
 # Blocks
 print_balance_block_header "OSMOSIS" "$OSMO_ADDRESS" 810
 print_balance_block_value "uosmo" "$(get_osmosis_balance uosmo)" 810
-print_balance_block_value "naan" "$(get_osmosis_balance $NAM_IBC)" 810
+print_balance_block_value "$NAM_DENOM" "$(get_osmosis_balance $NAM_IBC)" 810
 echo ""
 print_balance_block_header "NAMADA (TRANSPARENT)" "$NAM_TRANSPARENT" 1500
-print_balance_block_value "naan" "$(get_namada_transparent_balance "$NAM_DENOM")" 1500
+print_balance_block_value "$NAM_DENOM" "$(get_namada_transparent_balance "$NAM_DENOM")" 1500
 print_balance_block_value "uosmo${NAM_CHANNEL:+ ($NAM_CHANNEL)}" "$(get_namada_transparent_balance "$NAM_UOSMO_DENOM")" 1500
 echo ""
 print_balance_block_header "NAMADA (SHIELDED)" "$(shorten_address "$NAM_VIEWING_KEY" 21 21)" 795
-print_balance_block_value "naan" "$(get_namada_shielded_balance "$NAM_DENOM")" 795
+print_balance_block_value "$NAM_DENOM" "$(get_namada_shielded_balance "$NAM_DENOM")" 795
 print_balance_block_value "uosmo${NAM_CHANNEL:+ ($NAM_CHANNEL)}" "$(get_namada_shielded_balance "$NAM_UOSMO_DENOM")" 795
 echo ""
 
